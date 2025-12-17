633a16c00c04727f1aa22e7cc64c4a0d23c8c52a
David <loorenz.david@gmail.com>
2025-12-17 11:14:24 +0100

    added procfile for deploying with socket server

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 from Delivery_app_BK import create_app
+from Delivery_app_BK.socketio_instance import socketio
 
 application = create_app("production")  
 
-
+if __name__ == "__main__":
+    socketio.run(application, host='0.0.0.0', port=5000)
