8c5d17c3e2c4c444a2bb07922b47e769de02666d
David <loorenz.david@gmail.com>
2025-12-20 07:59:20 +0100

    changed directory for google credentials

--- .platform/hooks/predeploy/01_copy_credentials.sh
+++ .platform/hooks/predeploy/01_copy_credentials.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Create credentials directory inside staging
-mkdir -p /var/app/staging/credentials
+mkdir -p /var/app/current/credentials
 
 # Write JSON from environment variable into a file
-echo "$GOOGLE_APPLICATION_JSON" > /var/app/staging/credentials/google_service_account.json
+echo "$GOOGLE_APPLICATION_JSON" > /var/app/current/credentials/google_service_account.json
 
 # Set secure permissions
-chmod 600 /var/app/staging/credentials/google_service_account.json
+chmod 600 /var/app/current/credentials/google_service_account.json
