8ae91e272e4a816890473364b28078832941f733
David <loorenz.david@gmail.com>
2025-12-20 14:08:24 +0100

    added application socket

--- application.py
+++ application.py
@@ -5,7 +5,6 @@ from Delivery_app_BK.socketio_instance import socketio
 
 application = create_app("production")  
 
-app = socketio.WSGIApp(application)
 
 if __name__ == "__main__":
     socketio.run(application, host='0.0.0.0', port=8000, logger=True, engineio_logger=True)
