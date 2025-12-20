812eec5efea85af6e46f9354c29ad9b93fd37fef
David <loorenz.david@gmail.com>
2025-12-20 07:31:50 +0100

    added exec for eb deploy

--- .platform/hooks/predeploy/01_copy_credentials.sh
+++ .platform/hooks/predeploy/01_copy_credentials.sh
@@ -4,6 +4,6 @@
 mkdir -p /var/app/staging/credentials
 
 # Copy your local file (included in the deployment bundle)
-cp -f Back_end/credentials/google_service_account.json /var/app/staging/credentials/google_service_account.json
+cp -f credentials/google_service_account.json /var/app/staging/credentials/google_service_account.json
 
 chmod 600 /var/app/staging/credentials/google_service_account.json
