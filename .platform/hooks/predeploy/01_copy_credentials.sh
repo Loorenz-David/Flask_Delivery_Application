df2826c9bc8210cf16cb6cad7ab90149492fa67e
David <loorenz.david@gmail.com>
2025-12-20 07:44:59 +0100

    added sh script for EB

--- .platform/hooks/predeploy/01_copy_credentials.sh
+++ .platform/hooks/predeploy/01_copy_credentials.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Create credentials folder inside the deployed app
+# Create credentials directory inside staging
 mkdir -p /var/app/staging/credentials
 
-# Copy your local file (included in the deployment bundle)
-cp -f credentials/google_service_account.json /var/app/staging/credentials/google_service_account.json
+# Write JSON from environment variable into a file
+echo "$GOOGLE_APPLICATION_JSON" > /var/app/staging/credentials/google_service_account.json
 
+# Set secure permissions
 chmod 600 /var/app/staging/credentials/google_service_account.json
